0c2dc9157fe5a2238c6b079c2b5a562c27d6a219
marc <jones.marc@gmail.com>
2019-02-09 15:36:48 +0000

    adding check for git curl

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installing marcs-vim..."
-$ type git >/dev/null 2>&1 || { echo >&2 "git not installed.. exiting"; exit 1; }
-$ type curl >/dev/null 2>&1 || { echo >&2 "curl not installed.. exiting"; exit 1; }
+type git >/dev/null 2>&1 || { echo >&2 "git not installed.. exiting"; exit 1; }
+type curl >/dev/null 2>&1 || { echo >&2 "curl not installed.. exiting"; exit 1; }
 DIR=`dirname $0`
 mv ~/.vimrc ~/.vimrc_old
 ln -s ${DIR}/.vimrc ~/.vimrc
